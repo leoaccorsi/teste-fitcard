a1676b98857fff3a0794fc5de48b29031ce7cd21
Pudim <leonardo.accorsi@hotmail.com>
2018-10-29 22:06:28 -0300

    Criar estrutura do projeto

---
+++ TestePraticoModel/Model/EstabelecimentoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestePraticoModel.Model
+{
+    public class EstabelecimentoModel
+    {
+        public long id { get; set; }
+        public string razao_social { get; set; }
+        public string nome_fantasia { get; set; }
+        public string cnpj { get; set; }
+        public string email { get; set; }
+        public string endereco { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; }
+        public DateTime data_cadastro { get; set; }
+        public long cod_categoria { get; set; }
+        public Int16 status { get; set; }
+        public string agencia { get; set; }
+        public string conta { get; set; }
+    }
+}
