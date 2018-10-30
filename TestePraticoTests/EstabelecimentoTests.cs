840929cb38af5309b3695fe82aebd69f6e6180c1
Pudim <leonardo.accorsi@hotmail.com>
2018-10-29 22:12:14 -0300

    Recriar as classes de testes pois foram criadas erradas

---
+++ TestePraticoTests/EstabelecimentoTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestePraticoTests
+{
+    [TestClass]
+    public class EstabelecimentoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
